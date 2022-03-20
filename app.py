af4a51585c3be545a97a4b70f60d71ddcf07b9fe
Christopher Mitchell <christopher_m_mitchell@live.co.uk>
2022-03-20 12:26:09 +0000

    fixed SQLAlchemy import

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from flask import Flask, render_template, url_for
-from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column
 
 @app.route('/')
 def hello_world():  # put application's code here
