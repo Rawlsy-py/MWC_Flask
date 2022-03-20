c73d4816db4773d57a903f7cb7a06900bf92114e
Christopher Mitchell <christopher_m_mitchell@live.co.uk>
2022-03-20 10:33:35 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
